84f0b1d80b67d8112279831e81bc77d57caeb069
samar ben hadj abbes <samarabbes3@gmail.com>
2021-11-21 16:05:30 +0100

    Initial commit

---
+++ app/src/main/java/com/samar/upload_image/Uplode_Reg_Photo.java
@@ -0,0 +1,14 @@
+package com.samar.upload_image;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Uplode_Reg_Photo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_reg_photo);
+    }
+}
